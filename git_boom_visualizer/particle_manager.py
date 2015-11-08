b2408f5cf22e17d3b5b9bdb2df3808fa7c371201
David Bergström <david-bergstrom@outlook.com>
2015-11-08 12:15:54 +0100

    Seperate ParticleManager into seperate source file

---
+++ git_boom_visualizer/particle_manager.py
@@ -0,0 +1,57 @@
+from random import Random
+from math import cos, sin, pi
+
+class Particle:
+    def __init__(self, x, y, angle, angular_velocity, ttl, size):
+        self.x = x
+        self.y = y
+        self.angle = angle
+        self.angular_velocity = angular_velocity
+        self.ttl = ttl
+        self.size = size
+    
+    def update(self, time):
+        self.ttl -= 1
+        self.x += self.angular_velocity * cos(self.angle)
+        self.y += self.angular_velocity * sin(self.angle)
+        
+        return self.ttl > 0
+
+    def get_pos(self):
+        return (int(self.x), int(self.y))
+
+    def shift_center(self, pos, image):
+        shift = (- image.get_width() / 2.0, - image.get_height() / 2.0)
+        return map(sum, zip(pos, shift))
+
+    def draw(self, screen, texture):
+        pos = self.get_pos()
+        screen.blit(texture, self.shift_center(pos, texture))
+
+class ParticleManager():
+    def __init__(self, particle_count, texture):
+        self.particle_count = particle_count
+        self.texture = texture
+
+        self.rand = Random()
+        self.particles = []
+        for i in range(particle_count):
+            self.particles.append(self.random_particle())
+
+    def update(self, ms_elapsed, boost):
+        self.particles = filter(lambda particle :
+                                particle.update(ms_elapsed),
+                                self.particles)
+        
+        for i in range(self.particle_count - len(self.particles) + boost):
+            self.particles.append(self.random_particle(boost))
+
+    def draw(self, screen):
+        for particle in self.particles:
+            particle.draw(screen, self.texture)
+
+    def random_particle(self, boost=0):
+        randint = self.rand.randint
+        return Particle(randint(100, 500), randint(470, 480),
+                        self.rand.uniform(-0.3, 0.3) - pi / 2,
+                        randint(1, 5), randint(5, 60), 100)
